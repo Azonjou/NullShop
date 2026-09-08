c3892c7818b3cec46ad51c57f876ff27f72715a2
Tema <abatmanov2007@gmail.com>
2026-09-08 22:41:24 +0300

    Add database SQLite

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+#СТрока подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+#Создаем engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Настраивеам фабрику сеансов
+Session = sessionmaker(bind=engine)
+
+# Определяем базовый класс для моделей
+class Base(DeclarativeBase):
+    pass
