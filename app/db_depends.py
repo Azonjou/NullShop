f748db892b84186996ba1523a38aa807e1f05e5e
Tema <abatmanov2007@gmail.com>
2026-09-19 22:39:18 +0300

    update db_depends file

--- app/db_depends.py
+++ app/db_depends.py
@@ -14,3 +14,14 @@ def get_db() -> Generator[Session, None, None]:
         yield db #возвращает сессию
     finally:
         db.close()
+
+# --------------- Асинхронная сессия -------------------------
+
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import async_session_maker
+
+async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
+    """Предоставляет асинхронную сессию SQLAlchemy для работы с бд"""
+    async with async_session_maker() as session:
+        yield session  
